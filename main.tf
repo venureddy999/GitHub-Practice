b50c4390732da8141d02ad57816fe3abe8afa64e
venureddy909 <venu.unix.com>
2021-01-12 20:10:46 -0600

    Added AWS VPC Info

--- main.tf
+++ main.tf
@@ -4,3 +4,12 @@ provider "aws" {
     secret_key = "fOx09Y9ZhAo/5uFgmnvPt9XX5BO9JGMd9UmFcCOj"
     region = "us-east-1"
 }
+
+resource "aws_vpc" "default" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "DevOps-VPC"
+	Owner = "Venu"
+	    }
+}
