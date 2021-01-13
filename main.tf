f74fcfdd149f08c50aa363aacd9bdf56c696bb58
venureddy909 <venu.unix.com>
2021-01-12 20:07:12 -0600

    Added AWS Cloud Access

---
+++ main.tf
@@ -0,0 +1,6 @@
+#This Terraform Code Deploys Basic VPC Infra.
+provider "aws" {
+    access_key = "AKIA4WSPHBOJ7Y5P7TG5"
+    secret_key = "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"
+    region = "us-east-1"
+}
